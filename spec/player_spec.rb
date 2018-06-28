14cf38d656f1be88b2f156c891ddc84c31d4c0ce
Jay Khan <jkhan992@gmail.com>
2018-06-28 10:33:38 +0100

    updated Player test

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,9 +1,11 @@
 describe Player do
 
-subject(:jay) {described_class.new('Jay')}
+  describe '#initialize' do
+    subject(:jay) {described_class.new('Jay')}
 
-  it 'returns its own name' do
-    expect(jay.name).to eq "Jay"
+      it 'returns its own name' do
+        expect(jay.name).to eq "Jay"
+      end
   end
 
 end
