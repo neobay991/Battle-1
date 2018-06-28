25f5cc2c414e300d7265645a664f8b27cee7bb77
Jay Khan <jkhan992@gmail.com>
2018-06-28 10:38:46 +0100

    updated logic to show player 2 hit points instead of player 1

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -3,6 +3,6 @@ require "./app.rb"
 feature "hit_points" do
   scenario "see player 2 hit points" do
     sign_in_and_play
-    expect(page).to have_content 'Jay: 60HP'
+    expect(page).to have_content 'Daniel: 60HP'
   end
 end
