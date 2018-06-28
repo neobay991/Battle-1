89580eaeb34c76268d1799f5c808d78a33bea8a0
Jay Khan <jkhan992@gmail.com>
2018-06-28 18:49:05 +0100

    made controller skinny, updated tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,14 +1,26 @@
 require 'game'
 
 describe Game do
-  subject(:game) {Game.new}
-  let(:jay) {Player.new('Jay')}
-  let(:daniel) {Player.new('Daniel')}
+  subject(:game) { described_class.new(player_1, player_2) }
+   let(:player_1) { double :player }
+   let(:player_2) { double :player }
+
+  describe '#player1' do
+    it 'it retrieves player 1' do
+      expect(game.player_1).to eq player_1
+    end
+  end
+
+  describe '#player2' do
+    it 'it retrieves player 2' do
+      expect(game.player_2).to eq player_2
+    end
+  end
 
   describe '#attack' do
     it 'damages the player' do
-      expect(daniel).to receive(:receive_damage)
-      game.attack(daniel)
+      expect(player_2).to receive(:receive_damage)
+      game.attack(player_2)
     end
   end
 end
