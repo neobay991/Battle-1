82825b481027768e166568a6b23f665291f5e106
Jay Khan <jkhan992@gmail.com>
2018-06-28 11:08:27 +0100

    added hit points tests and feature

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,14 @@
 class Player
-
-  attr_reader :name
+  DEFAULT_HIT_POINTS = 60
+  attr_reader :name, :hp_counter
 
   def initialize(name)
     @name = name
+    @hp_counter = 60
+  end
+
+  def hit_points
+    @hp_counter = DEFAULT_HIT_POINTS
   end
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,11 +1,16 @@
 describe Player do
+  subject(:jay) {described_class.new('Jay')}
 
   describe '#initialize' do
-    subject(:jay) {described_class.new('Jay')}
-
       it 'returns its own name' do
         expect(jay.name).to eq "Jay"
       end
+  end
+
+  describe "#hit_points" do
+    it "returns the hit points" do
+      expect(jay.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+    end
   end
 
 end
