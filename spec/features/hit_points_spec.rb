68e42934e42aa36cdccbcbe4361d44d9702a5849
Jay Khan <jkhan992@gmail.com>
2018-06-29 10:28:30 +0100

    added tests and hit points for player 1

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,8 +1,21 @@
 require "./app.rb"
 
-feature "hit_points" do
-  scenario "see player 2 hit points" do
+feature 'hit_points' do
+
+  # As Player 1,
+  # So I can see how close I am to winning
+  # I want to see Player 2's Hit Points
+  scenario 'see player 2 hit points' do
     sign_in_and_play
     expect(page).to have_content 'Daniel: 60HP'
   end
+
+  # As Player 1,
+  # So I can see how close I am to losing,
+  # I want to see my own hit points
+  scenario 'see player 1 hit points' do
+    sign_in_and_play
+    expect(page).to have_content 'Jay: 60HP'
+  end
+
 end
